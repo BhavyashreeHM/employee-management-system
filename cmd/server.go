a4cdef8065cc9f71d6a6862835ed1bedea628781
BhavyashreeHM <bhavyashreehm@gmail.com>
2026-01-27 10:25:35 +0000

    Gin Server Running on PORT

--- cmd/server.go
+++ cmd/server.go
@@ -1,1 +1,16 @@
-package cmd
+package main
+
+import (
+	"employee-management-system/internal/api/handlers"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := "8080"
+	r := gin.Default()
+	r.GET("/", handlers.Emploee)
+	log.Println("Server running on port", port)
+	r.Run(":" + port)
+}
--- internal/api/handlers/emploee.go
+++ internal/api/handlers/emploee.go
@@ -1,1 +1,14 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Emploee(c *gin.Context) {
+	// Return JSON response
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	})
+}
